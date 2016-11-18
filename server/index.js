9abfaa228e23470c09afc6fa720606b01eaac29c
Hector Coto Rojas <heavenhell64@gmail.com>
2016-11-18 09:52:28 -0600

    App setup complete

--- server/index.js
+++ server/index.js
@@ -2,12 +2,17 @@
 
 const express = require('express');
 const http = require('http');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const bodyParser = require('body-parser'); //Used for parsing incoming request, JSON in this case
+const morgan = require('morgan'); //logging framework, mostly just for debugging.
 
 const app = express();
+const router = require('./router');
 
 // App setup
+/* These two serves as middleware, kind of a filther like */
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
 
 // Server setup
 const port = process.env.PORT || 3090;
