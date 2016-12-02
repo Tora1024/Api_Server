c39b1873cac251d9a97f43ad703582d0edfd9a92
Hector Coto Rojas <heavenhell64@gmail.com>
2016-12-01 21:31:23 -0600

    Final details, finished the circle of encryption / decryption of password for either sign up / sign in. Also made the proper route for signing in. Just need to take care of a tiny error.

--- server/router.js
+++ server/router.js
@@ -1,8 +1,15 @@
 const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+// Since we are using jwt, we need to turn off the default behavior of creating a cookie session
+const requireAuth = passport.authenticate('jwt', { session: false });
+const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function (app) {
-	app.get('/', function (req, res, next) {
-		res.send(['water', 'bottle', 'paper']);
+	app.get('/', requireAuth, function (req, res) {
+		res.send({ hi: 'there' });
 	});
+	app.post('/signin', requireSignin, Authentication.signin);
 	app.post('/signup', Authentication.signup);
 };
--- server/services/passport.js
+++ server/services/passport.js
@@ -3,22 +3,51 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+
+// Create local strategy
+const localOptions = { usernameField: 'email' };
+const localLogin = new LocalStrategy({ localOptions }, function (email, password, done) {
+	// Verify this email and password, call done with the user
+	// if it is the correct email and password,
+	// otherwise, call done with false
+	User.findOne({ email: email }, function (err, user) {
+		if (err) {
+			return done(err);
+		}
+		if (!user) {
+			return done(null, false);
+		}
+
+		// Compare passwords - is `password` equal to user.pasword?
+		user.comparePassword(password, function (err, isMatch) {
+			if (err) {
+				return done(err);
+			}
+			if (!isMatch) {
+				return done(null, false);
+			}
+
+			return done(null, user);
+		});
+	});
+});
 
 // Setup options for JWT Strategy
 const jwtOptions = {
-	jwtFromRquest: ExtractJwt.fromHeader('authorization'),
+	jwtFromRequest: ExtractJwt.fromHeader('authorization'),
 	secretOrKey: config.secret
 };
 
 // Create JWT strategy
-const jwtlogin = new JwtStrategy(jwtOptions, function(payload, done) {
+const jwtlogin = new JwtStrategy(jwtOptions, function (payload, done) {
 	// Payload is the decoded JWT token, with a sub property and issued time property.
 	// Done is a callback function to see if an user was or wasnt successfully authenticated.
 
 	// We wnt to see if the User ID in the payload exists in our database
 	// If it does, call 'done' with that ID
 	// otherwise, call done without the user object
-	User.finById(payload.sub, function(err, user){
+	User.findById(payload.sub, function (err, user) {
 		if (err) {
 			return done(err, false);
 		}
@@ -33,3 +62,4 @@ const jwtlogin = new JwtStrategy(jwtOptions, function(payload, done) {
 
 // Tell passport to use this strategy
 passport.use(jwtlogin);
+passport.use(localLogin);
